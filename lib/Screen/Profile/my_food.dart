042854146862f2f8600b21fc94c189d7dfce1ea2
Worapong Chaiyabung <109983974+worapong2001@users.noreply.github.com>
2023-08-16 22:07:19 +0700

    upload & Update

--- lib/Screen/Profile/my_food.dart
+++ lib/Screen/Profile/my_food.dart
@@ -1,3 +1,324 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:se_project_food/Authen/authen_part.dart';
+// import 'package:se_project_food/Edit/editfood_page.dart';
+// import 'package:se_project_food/Screen/Profile/user_profile.dart';
+
+// import '../../Models/foodmodels.dart';
+
+// class MyFoods extends StatefulWidget {
+//   const MyFoods({super.key});
+
+//   @override
+//   State<MyFoods> createState() => _MyFoodsState();
+// }
+
+// class _MyFoodsState extends State<MyFoods> {
+//   List<FoodModel> foodModels = [];
+//   final TextEditingController edit_name = TextEditingController();
+//   final TextEditingController edit_description = TextEditingController();
+//   final TextEditingController edit_ingredients = TextEditingController();
+//   //Method ที่ทำงาน อ่านค่าที่อยู่ใน fire store
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     readData();
+//   }
+
+// // _HomeFeedState(String id, {Key? key}) : super(key: key) {
+// //    // _documentReference = FirebaseFirestore.instance.collection('Foods').doc(id);
+// //    // _future = _documentReference.get();
+// //   }
+
+//   Future<void> readData() async {
+//     //read data
+//     FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+//     CollectionReference collectionReference =
+//         firestore.collection('Foods'); //collection Person  //await
+//     await collectionReference.snapshots().listen((response) {
+//       List<DocumentSnapshot> snapshots =
+//           response.docs; //snapshot from firestore [array]
+//       for (var snapshot in snapshots) {
+//         //print("object");
+//         FoodModel foodModel =
+//             FoodModel.fromMap(snapshot.data() as Map<String, dynamic>);
+//         foodModel.food_id = snapshot.id;
+
+//         setState(() {
+//           //print("object");
+//           if (user?.uid == foodModel.user_id) {
+//             foodModels.add(foodModel);
+//           }
+//         });
+//       }
+//     });
+//   }
+
+//   //logout *
+//   Future<void> signOut() async {
+//     await AuthenticationController().signOut;
+//   }
+
+//   //Widget Show Current User *
+//   Widget _userUID() {
+//     return Text(user?.email ?? 'User email');
+//   }
+
+//   //Widget Logout Button *
+//   Widget _signOutButton() {
+//     return ElevatedButton(
+//       style: ElevatedButton.styleFrom(
+//           primary: Colors.red,
+//           textStyle: TextStyle(
+//             fontSize: 15,
+//             fontWeight: FontWeight.bold,
+//           )),
+//       onPressed: signOut,
+//       child: const Text('ออกจากระบบ'),
+//     );
+//   }
+
+//   Widget showImage(int index) {
+//     return Container(
+//       width: MediaQuery.of(context).size.width * 0.5,
+//       height: MediaQuery.of(context).size.width * 0.5,
+//       child: Image.network(
+//         foodModels[index].food_image,
+//       ),
+//     );
+//   }
+
+//   Widget showName(int index) {
+//     return Text(
+//       foodModels[index].food_name,
+//     );
+//   }
+
+//   Widget showDescription(int index) {
+//     return Text(
+//       foodModels[index].food_description,
+//     );
+//   }
+
+//   Widget showIngredients(int index) {
+//     return Text(
+//       foodModels[index].food_ingredients,
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Center(
+//           child: Container(
+//             child: ListView.builder(
+//               itemCount: foodModels.length,
+//               itemBuilder: (BuildContext buildContext, int index) {
+//                 return Card(
+//                   child: Column(children: <Widget>[
+//                     Text(''),
+//                     Container(
+//                         width: MediaQuery.of(context).size.width * 0.5,
+//                         height: MediaQuery.of(context).size.width * 0.5,
+//                         child: Image.network(
+//                           foodModels[index].food_image,
+//                         )),
+//                     showName(
+//                       index,
+//                     ),
+//                     showDescription(index),
+//                     showIngredients(index),
+
+//                     Center(
+//                       child: ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                             primary: Colors.amber,
+//                             textStyle: TextStyle(
+//                               fontSize: 15,
+//                               fontWeight: FontWeight.bold,
+//                             )),
+//                         onPressed: () {
+//                           print('Start NaJa');
+
+//                           print(foodModels[index].food_id);
+//                           Get.to(EditFoods(),
+//                               arguments: foodModels[index].food_id);
+//                           // showDialog<void>(
+//                           //   context: context,
+//                           //   barrierDismissible: false, // user must tap button!
+//                           //   builder: (BuildContext context) {
+//                           //     return AlertDialog(
+//                           //       title: const Text(''),
+//                           //       content: SingleChildScrollView(
+//                           //         child: ListBody(
+//                           //           children: <Widget>[
+//                           //             Text("ชื่อสูตรอาหาร : "),
+//                           //             SizedBox(
+//                           //               height: 10.0,
+//                           //             ),
+//                           //             TextFormField(
+//                           //               controller: edit_name,
+//                           //               decoration: InputDecoration(
+//                           //                 border: OutlineInputBorder(
+//                           //                     borderRadius:
+//                           //                         BorderRadius.circular(20)),
+//                           //               ),
+//                           //             ),
+//                           //             SizedBox(
+//                           //               height: 10.0,
+//                           //             ),
+//                           //             Text("วัตถุดิบ : "),
+//                           //             SizedBox(
+//                           //               height: 10.0,
+//                           //             ),
+//                           //             TextFormField(
+//                           //               controller: edit_ingredients,
+//                           //               decoration: InputDecoration(
+//                           //                 border: OutlineInputBorder(
+//                           //                     borderRadius:
+//                           //                         BorderRadius.circular(20)),
+//                           //               ),
+//                           //             ),
+//                           //             SizedBox(
+//                           //               height: 10.0,
+//                           //             ),
+//                           //             Text("วิธีการทำ : "),
+//                           //             SizedBox(
+//                           //               height: 10.0,
+//                           //             ),
+//                           //             TextFormField(
+//                           //               controller: edit_description,
+//                           //               decoration: InputDecoration(
+//                           //                 border: OutlineInputBorder(
+//                           //                     borderRadius:
+//                           //                         BorderRadius.circular(20)),
+//                           //               ),
+//                           //             ),
+//                           //           ],
+//                           //         ),
+//                           //       ),
+//                           //       actions: <Widget>[
+//                           //         TextButton(
+//                           //           child: const Text('ยืนยันการแก้ไข'),
+//                           //           onPressed: () {
+//                           //             late String _editname = edit_name.text;
+//                           //             late String _editingredients =
+//                           //                 edit_ingredients.text;
+//                           //             late String _editdescription =
+//                           //                 edit_description.text;
+//                           //             final docker = FirebaseFirestore.instance
+//                           //                 .collection('Foods')
+//                           //                 .doc(foodModels[index].food_id);
+//                           //             docker.update({
+//                           //               'Food_Description': _editdescription,
+//                           //               'Food_Ingredients': _editingredients,
+//                           //               'Food_Name': _editname,
+//                           //             });
+//                           //             Navigator.of(context).pop();
+//                           //           },
+//                           //         ),
+//                           //       ],
+//                           //     );
+//                           // },
+//                           //);
+//                         },
+//                         child: Text('แก้ไขข้อมูล'),
+//                       ),
+//                     ),
+//                     Center(
+//                       child: ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                             primary: Colors.redAccent,
+//                             textStyle: TextStyle(
+//                               fontSize: 15,
+//                               fontWeight: FontWeight.bold,
+//                             )),
+//                         onPressed: () {
+//                           print('Start NaJa');
+
+//                           print(foodModels[index].food_id);
+//                           // final DocumentSnapshot snapshot =
+//                           //     await FirebaseFirestore.instance
+//                           //         .collection("Foods")
+//                           //         .doc(foodModels[index].food_id)
+//                           //         .get();
+
+//                           // FirebaseFirestore firestore =
+//                           //     FirebaseFirestore.instance;
+
+//                           // //ทำการเรียกข้อมูลเอกสารจาก Firebase
+//                           // DocumentSnapshot sd = await firestore
+//                           //     .collection('Foods')
+//                           //     .doc(foodModels[index].food_id)
+//                           //     .get();
+
+//                           //ตรวจสอบว่ามีข้อมูลหรือไม่
+//                           // String? foodid;
+//                           // Map<String, dynamic>? data =
+//                           //     sd.data() as Map<String, dynamic>?;
+//                           // if (sd.exists) {
+//                           //   foodid = data!['Food_id'];
+//                           // }
+
+//                           final docker = FirebaseFirestore.instance
+//                               .collection('Foods')
+//                               .doc(foodModels[index].food_id);
+//                           //docker.delete();
+//                           docker.delete();
+//                           print('foodid = ');
+
+//                           //print(foodid!);
+
+//                           // final docker2 = FirebaseStorage.instance
+//                           //     .ref()
+//                           //     .child('files')
+//                           //     .child(foodid)
+//                           //     .child('Image')
+//                           //     .child('')
+//                           //     .delete();
+
+//                           // docker2.delete().then((_) {
+//                           //   print('ลบข้อมูลเรียบร้อยแล้ว');
+//                           // }).catchError((error) {
+//                           //   if (error is FirebaseException &&
+//                           //       error.code == 'object-not-found') {
+//                           //     print('ไม่พบข้อมูลที่ต้องการลบ');
+//                           //   } else {
+//                           //     print('เกิดข้อผิดพลาดในการลบข้อมูล: $error');
+//                           //   }
+//                           // });
+//                         },
+//                         child: Text('ลบข้อมูล'),
+//                       ),
+//                     ),
+//                     // IconButton(
+//                     //   icon: new Icon(Icons.edit),
+//                     //   highlightColor: Colors.pink,
+//                     //   onPressed: () {
+//                     //     print("kuay");
+//                     //     MaterialPageRoute route = MaterialPageRoute(
+//                     //       builder: (Index) => UploadFoodPage(),
+//                     //     );
+//                     //   },
+//                     // ),
+//                   ]),
+//                 );
+//               },
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+
+//     //Stream<List<PersonModel>> readData
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
