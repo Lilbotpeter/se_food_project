ef3f5d79a800b5f93bc9c1dbfcfa1555eaa679c9
Worapong Chaiyabung <109983974+worapong2001@users.noreply.github.com>
2023-09-16 23:21:38 +0700

    admin logon

--- lib/Admin/AdminPage.dart
+++ lib/Admin/AdminPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../Authen/authen_part.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({super.key});
@@ -8,8 +11,31 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
+  Future<void> signOut() async {
+    await AuthenticationController().signOut;
+  }
+
+  Widget _signOutButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: Colors.red,
+          textStyle: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          )),
+      onPressed: signOut,
+      child: const Text('ออกจากระบบ'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Kuay');
+    return _signOutButton();
+    // Container(
+    //   child: Row(children: [
+    //   //   TextButton(
+    //   //       onPressed: _signOutButton, child: const Text('ออกจากระบบนะจ๊ะ'))
+    //   // ]),
+    // );
   }
 }
--- lib/Authen/authen_part.dart
+++ lib/Authen/authen_part.dart
@@ -7,100 +7,99 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:se_project_food/Screen/Login.dart';
 import 'package:se_project_food/Screen/Register.dart';
+import '../Admin/AdminPage.dart';
 import '../Models/user.dart' as usermodel;
 import '../Screen/home.dart';
 import '../global.dart';
 
-class AuthenticationController extends GetxController{
-
-  static AuthenticationController instanceAuth = Get.put(AuthenticationController());
+class AuthenticationController extends GetxController {
+  static AuthenticationController instanceAuth =
+      Get.put(AuthenticationController());
   late Rx<User?> _currentUser;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   //final User? user = AuthenticationController().currentUser;
 
-  
   late Rx<File?> _pickedFile;
-
 
   File? get profileImage => _pickedFile.value;
 
   //Choose image from gallery เด้อ
-  void chooseImageFromGallery() async{
-    final pickedImageFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  void chooseImageFromGallery() async {
+    final pickedImageFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
 
-    if(pickedImageFile != null){
+    if (pickedImageFile != null) {
       Get.snackbar(
         "เลือกภาพสำเร็จ",
         "เพิ่มภาพโปรไฟล์เรียบร้อยแล้ว",
-        );
+      );
     }
 
-  _pickedFile = Rx<File?>(File(pickedImageFile!.path));
+    _pickedFile = Rx<File?>(File(pickedImageFile!.path));
   }
 
 //Choose image from ก้อง เด้อ
-void captureImageWithCamera() async{
-    try{
-    final pickedImageFile = await ImagePicker().pickImage(source: ImageSource.camera);
+  void captureImageWithCamera() async {
+    try {
+      final pickedImageFile =
+          await ImagePicker().pickImage(source: ImageSource.camera);
 
-    if(pickedImageFile != null){
-      Get.snackbar(
-        "ถ่ายภาพสำเร็จ",
-        "เพิ่มภาพโปรไฟล์เรียบร้อยแล้ว",
+      if (pickedImageFile != null) {
+        Get.snackbar(
+          "ถ่ายภาพสำเร็จ",
+          "เพิ่มภาพโปรไฟล์เรียบร้อยแล้ว",
         );
-    }
+      }
 
-  _pickedFile = Rx<File?>(File(pickedImageFile!.path));
-    }
-    catch(error){
+      _pickedFile = Rx<File?>(File(pickedImageFile!.path));
+    } catch (error) {
       Get.snackbar("อัพโหลดรูปไม่สำเร็จ", 'กรุณาเพิ่มรูปภาพใหม่อีกครั้ง');
     }
-    
   }
 
   //createAccountForNewUser
-  void createAccountForNewUser(File imageFile,String username,String userEmail,String userPassword,String userPhone) async {
-    try{
-        //1. create user in the firebase authentication
-        UserCredential credential = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(
-          email: userEmail,
-          password: userPassword,
-          );
+  void createAccountForNewUser(File imageFile, String username,
+      String userEmail, String userPassword, String userPhone) async {
+    try {
+      //1. create user in the firebase authentication
+      UserCredential credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: userEmail,
+        password: userPassword,
+      );
 
       //2.save the user profile image to firebase storage
       String imageDowloadUrl = await uploadImageToStorage(imageFile);
 
       //3.save user data to the firestore database
       usermodel.User user = usermodel.User(
-        // email: userEmail,
-        // pathImage: imageDowloadUrl,
-        // name = username,
-        // phone = userPhone,
-        // uid = credential.user!.uid,
-        email: userEmail,
-        name: username, 
-        pathImage: imageDowloadUrl,
-        phone: userPhone,
-        uid: credential.user!.uid
-      );
+          // email: userEmail,
+          // pathImage: imageDowloadUrl,
+          // name = username,
+          // phone = userPhone,
+          // uid = credential.user!.uid,
+          email: userEmail,
+          name: username,
+          pathImage: imageDowloadUrl,
+          phone: userPhone,
+          uid: credential.user!.uid);
 
-      await FirebaseFirestore.instance.collection("users").doc(credential.user!.uid).set(user.toJson());
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(credential.user!.uid)
+          .set(user.toJson());
       Get.snackbar("สมัครสมาชิกสำเร็จ", "บัญชีของคุณถูกสร้างสำเร็จแล้ว");
+    } catch (error) {
+      Get.snackbar("สมัครสมาชิกไม่สำเร็จ", "โปรดลองใหม่อีกครั้ง");
+      showProgressBar = false;
     }
-    catch(error){
-        Get.snackbar("สมัครสมาชิกไม่สำเร็จ", "โปรดลองใหม่อีกครั้ง");
-        showProgressBar = false;
-    }
-      
-
-
   }
 
-  Future<String> uploadImageToStorage(File imageFile) async{
-    Reference reference = FirebaseStorage.instance.ref()
-    .child("Profile Picture")
-    .child(FirebaseAuth.instance.currentUser!.uid);
+  Future<String> uploadImageToStorage(File imageFile) async {
+    Reference reference = FirebaseStorage.instance
+        .ref()
+        .child("Profile Picture")
+        .child(FirebaseAuth.instance.currentUser!.uid);
 
     UploadTask uploadTask = reference.putFile(imageFile);
     TaskSnapshot taskSnapshot = await uploadTask;
@@ -112,41 +111,40 @@ void captureImageWithCamera() async{
 
   //Login function
 
-  void loginUserNow(String userEmail,String userPassword) async {
-    try{
+  void loginUserNow(String userEmail, String userPassword) async {
+    try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: userEmail,
         password: userPassword,
-        );
+      );
       Get.snackbar("เข้าสู่ระบบสำเร็จ", "ยินดีต้อนรับ");
 
-      showProgressBar=false;
-    }
-    catch(error){
+      showProgressBar = false;
+    } catch (error) {
       Get.snackbar("เข้าสู่ระบบไม่สำเร็จ", "โปรดลองใหม่อีกครั้ง");
 
-      showProgressBar=false;
+      showProgressBar = false;
       Get.to(RegisterScreen());
     }
-
   }
 
   //Each Screen
-  goToScreen(User? currentUser){
+  goToScreen(User? currentUser) {
     //when user isn't already logged-in
-    if(currentUser == null){
+    print('currentUser = ');
+    print(currentUser);
+    if (currentUser!.email == null) {
       Get.offAll(LoginScreen());
+    } else if (currentUser!.email == 'admin@admin.com') {
+      Get.offAll(AdminPage());
     }
     //when user is already logged-in
-    else{
+    else {
       Get.offAll(HomeScreen());
-
     }
   }
 
   User? get currentUser => _firebaseAuth.currentUser;
-
-   
 
   @override
   void onReady() {
@@ -158,13 +156,7 @@ void captureImageWithCamera() async{
     ever(_currentUser, goToScreen);
   }
 
-    void signOut() async {
+  void signOut() async {
     await _firebaseAuth.signOut();
   }
-
-
-  
-  
-
-
 }
